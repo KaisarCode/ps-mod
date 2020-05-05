bf8404e756ca83b001db8c3d68d09179c45dbe81
KaisarCode <kaisar@kaisarcode.com>
2020-05-05 18:59:12 -0300

    change text in JS

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,2 @@
 /*PS Mod - 2020 KaisarCode*/
-(function(){ var x = 'psmod';console.log('Init APP'); })();
+(function(){ var x = 'psmod';console.log('Init PSMod'); })();
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
 <?
-console.log('Init APP');
+console.log('Init PSMod');
 ?>
