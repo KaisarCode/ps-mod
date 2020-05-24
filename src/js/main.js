eb5b0403796b2f4068a2385adc6e87486562d210
KaisarCode <kaisar@kaisarcode.com>
2020-05-24 19:32:16 -0300

    add submenu in modules

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
 <?
-console.log('Init PSMod');
-console.log(cfg);
+// Create submenu in modules
+jQuery(document).ready(function(){
+    
+    jQuery('\
+    #maintab-AdminParentModules .submenu,\
+    #subtab-AdminParentModulesSf .submenu,\
+    #collapse-43\
+    ').append('\
+    <li class="link-leveltwo">\
+        <a class="link" href="{$mod->url_conf|escape:'htmlall':'UTF-8'}">{$mod->displayName|escape:'htmlall':'UTF-8'}</a>\
+    </li>\
+    ');
+    
+});
 ?>
